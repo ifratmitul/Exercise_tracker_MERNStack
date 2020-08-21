a0f3b5eacf96b0f5d1969e0df94cd5a0c5760c53
ifratmitul <ifrat.mitul@hotmail.com>
2020-08-22 02:09:55 +0600

    front-end work 90% complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-
-import './App.css';
-import Dataloder from './DataLoader'
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Navbar from "./Component/Navbar";
+import CreateExercise from './Component/create-exercise';
+import CreateUser from './Component/create-user';
+import EditExercise from './Component/edit-exercise';
+import ExerciseList from './Component/Exercises-list';
 function App() {
   return (
-    <div className="App">
-      <Dataloder/>
-    </div>
+    <Router>
+       <Navbar />
+      <br/>
+       <Route path= "/" exact component ={ExerciseList}/> 
+      <Route path= "/edit/:id" exact component ={EditExercise}/>
+      <Route path= "/create" exact component ={CreateExercise}/>
+      <Route path= "/user" exact component ={CreateUser}/> 
+    </Router>
+    
   );
 }
 
